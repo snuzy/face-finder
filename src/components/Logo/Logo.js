f9762f68c0653c64dfc1843677291d4613e08a7a
Sharon <snuzyfe@yahoo.co.uk>
2018-06-18 17:14:13 +0100

    Update Logo.js
    logo updated

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import brain from './brain.png';
+import logo1 from './logo1.png';
 import './Logo.css';
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 100, width: 125 }} >
- 				<div className="Tilt-inner"><img className='logo' alt='logo' src={brain} /> </div>
+ 				<div className="Tilt-inner"><img className='logo' alt='logo' src={logo1} /> </div>
 			</Tilt>
 		</div>
 	);
